e996cb6d4e0ed12ddbe855d28bf134610e5bf4ed
HuyThanh8 <114084113+HuyThanh8@users.noreply.github.com>
2022-12-27 03:16:31 +0700

    1 file modified

--- lesson6/task.js
+++ lesson6/task.js
@@ -1,3 +1,29 @@
-const task = [];
+const tasks = [];
 
-function 
+function addNewTask(title, description) {
+  const newTask = {
+    title: title,
+    description: description,
+    status: uncomplete,
+  };
+  tasks.push(newTask);
+  return tasks.length;
+}
+
+addNewTask('Chạy bộ', '5h dậy, ra công viên chạy bộ');
+addNewTask('Đi chợ', '6h, đi chợ');
+addNewTask('Ăn sáng', '7h, làm bữa sáng ăn trước khi đi làm');
+addNewTask('Đi làm', '7h30, đến công ty làm việc');
+addNewTask('Đón con', '17h, đến trường đón con');
+addNewTask('Dọn dẹp nhà cửa', '18h, quét dọn, lau nhà, giặt đò');
+addNewTask('Nấu cơm', '19h, nấu cơm, ăn cơm');
+addNewTask('Đi ngủ', '23h, lên giường đi ngủ');
+
+function findTaskName(title) {
+  for (leti = 0; i < task.length; i++) {
+    if (tasks[i].title === title) {
+      return i;
+    }
+  }
+  return -1;
+}
